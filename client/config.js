e1e2505d245b26c9e883176d5b657c53311ef98a
Lunar <lunar@cypherpunk.is>
2017-10-12 13:43:49 -0500

    Update config.js

--- client/config.js
+++ client/config.js
@@ -1,4 +1,4 @@
 upload.config.server = '' // Empty if the webapp is in the same place as the server
 // upload.config.server = 'https://cipher.cx/' // If the webapp is separated from the server - remember the trailing slash
 upload.config.api_key = 'zbb9yez2bqa6wjvoz23btr6sjsir6rpv' // Should be the same as the server, used for uploading
-upload.config.footer = '<a href="https://github.com/lunarthegrey/cipher.cx" target="_blank">Source Code</a> - <a href="mailto:admin@cipher.cx" id="contact" target="_blank">Contact</a>'
+upload.config.footer = 'Upload limit = 50MB, files are deleted after 1 week <br /> <a href="https://github.com/lunarthegrey/cipher.cx" target="_blank">Source Code</a> - <a href="mailto:admin@cipher.cx" id="contact" target="_blank">Contact</a>'
