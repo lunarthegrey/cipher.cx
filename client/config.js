d78e1de613a103fa2a388733fd1150f16ec957be
Lunar <lunar@cypherpunk.is>
2017-10-12 13:08:36 -0500

    Update and rename config.js.example to config.js

---
+++ client/config.js
@@ -0,0 +1,4 @@
+upload.config.server = '' // Empty if the webapp is in the same place as the server
+// upload.config.server = 'https://cipher.cx/' // If the webapp is separated from the server - remember the trailing slash
+upload.config.api_key = 'zbb9yez2bqa6wjvoz23btr6sjsir6rpv' // Should be the same as the server, used for uploading
+upload.config.footer = '<a href="https://github.com/lunarthegrey/cipher.cx" target="_blank">Source Code</a> - <a href="mailto:admin@cipher.cx" id="contact" target="_blank">Contact</a>'
